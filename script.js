2365f192af079fc0fa26492629f6345796098a5c
Orlando LOQ <ojmonagas95@gmail.com>
2025-06-28 10:34:40 -0400

    Se elimina script innecesaria en root

--- script.js
+++
@@ -1,7 +0,0 @@
-const escalaMayorDeDo = [ "Do", "Re", "Mi", "Fa", "Sol", "La", "Si", ]
-let html = "";
-let salida = document.querySelector("#dynamic-content")
-for (let notas of escalaMayorDeDo)
-{ html += `${notas}` }
-salida.innerHTML = html
-
